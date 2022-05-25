617f0893c9b9a91315f767b0e3a434dd743c89bf
Brian <b.perez3237@gmail.com>
2022-05-24 22:56:58 -0400

    commit tree ecb41a31a3b8bb73b3f096b5cc94313df001e118

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+
+    def network 
+        self.last.network
+    end
+
+    def actors_list
+        list = []
+        self.actors.each {|x| list<<x.full_name}
+        list
+    end
 end
